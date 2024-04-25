722605c500179ab416289fa5c2a3093c1504d0a4
Mihyunee <esther2187@naver.com>
2024-04-25 12:09:15 +0900

    [추가] 스프링 시큐리티 적용

--- syFinal/src/main/java/com/example/syFinal/SyFinalApplication.java
+++ syFinal/src/main/java/com/example/syFinal/SyFinalApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SyFinalApplication {
@@ -32,4 +34,8 @@ public class SyFinalApplication {
 		return new SqlSessionTemplate(factory);
 	}
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
