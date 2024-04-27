efc8c174ca356a4cb0661e2b344ec1fe153dd3ce
Mihyunee <esther2187@naver.com>
2024-04-27 17:17:22 +0900

    [수정] 호텔테이블 컬럼 가져오기

--- syFinal/src/main/java/com/example/syFinal/host/controller/HotelController.java
+++ syFinal/src/main/java/com/example/syFinal/host/controller/HotelController.java
@@ -28,10 +28,14 @@ public class HotelController {
 		System.out.println("1. Controller list : " + list);
 		Map<String, Object> hotelList = new HashMap<>();
 		for (int i = 0; i < list.size(); i++) {
-			hotelList.put("ho_name", list.get(i).getHo_img());
+			hotelList.put("ho_name", list.get(i).getHo_name());
 			hotelList.put("ho_address", list.get(i).getHo_address());
 			hotelList.put("ho_level", list.get(i).getHo_level());
 			hotelList.put("ho_floor", list.get(i).getHo_floor());
+			hotelList.put("ho_single", list.get(i).getHo_single());
+			hotelList.put("ho_double", list.get(i).getHo_double());
+			hotelList.put("ho_family", list.get(i).getHo_family());
+			hotelList.put("ho_suite", list.get(i).getHo_suite());
 		}
 		System.out.println("2. Controller hotelList : " + hotelList);
 		List list2 = new ArrayList();
