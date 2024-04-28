9f81c7b1402887a394277a0c171f3f6a20cabd1f
YW <dusdn5774@naver.com>
2024-04-28 17:24:17 +0900

    [수정] 메인화면 수정

--- syFinal/src/main/java/com/example/syFinal/guest/model/dto/MainDTO.java
+++ syFinal/src/main/java/com/example/syFinal/guest/model/dto/MainDTO.java
@@ -1,5 +1,25 @@
 package com.example.syFinal.guest.model.dto;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class MainDTO {
+	private int ho_idx;
+	private String ho_name;
+	private String ho_img;
+	private MultipartFile img;
 	
+	public MainDTO(int ho_idx,String ho_name,String ho_img){
+		this.ho_idx=ho_idx;
+		this.ho_name=ho_name;
+		this.ho_img=ho_img;
+	}
 }
