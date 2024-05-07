0be1f13d961e810e245838e4c590988a01dd6062
mindy <92miindy@gmail.com>
2024-05-08 00:41:14 +0900

    [스타일] 호텔 상세 -> 숙소후기 리스트 스타일 적용. 테스트 중

--- frontend/src/pages/host/hotelDetailSection/Reputation.js
+++ frontend/src/pages/host/hotelDetailSection/Reputation.js
@@ -1,54 +1,96 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import Cookies from "universal-cookie";
 
-function useFetch(url) {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+import ReviewItem from "../../../component/ReviewItem";
 
-  useEffect(() => {
+function Reputation() {
+  const { HoIdx } = useParams();
+  const [list, setReviews] = useState([]);
+  // const [modal, setModal] = useState(false);
+  // const navigate = useNavigate();
+  const [avg, setAvg] = useState("");
+
+  // const cookies = new Cookies();
+  // //게스트 쿠키
+  // const g_idx = cookies.get("g_idx");
+  // const g_email = cookies.get("g_email"); //쿠키변수명
+  // //호스트 쿠키
+  // const userInfo = cookies.get("userInfo");
+
+  function getReviews(url) {
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-        console.log("===> data? " + JSON.stringify(data));
-        setData(data);
-        setLoading(false);
+        console.log("==> 리뷰 data? " + data);
+        setReviews(data.list);
+        setAvg(data.avg);
       });
+  }
+
+  useEffect(() => {
+    getReviews(`http://localhost/api/reputation/list/${HoIdx}`);
   }, []);
-  return [data, loading];
-}
 
-function ListReputation() {
-  const cookies = new Cookies();
-  const navigate = useNavigate();
-  const [list, setReviewList] = useState([]);
-  const searchKey = useRef();
-  const search = useRef();
-
-  // function getList(url) {
-  // }
-
-  // useEffect(()=>{("");}, []);
-
-  return (
-    <>
-      <div className="container card-style mb-30">
-        <div className="col-2"><hr2>평점</hr2></div>
-        <div className="col-10">
-          <div className="col-6"></div>
-          <div className="col-6"></div>
+  if (avg == null) {
+    console.log("==> avg null?" + (avg == null));
+    return (
+      <>
+        <div className="container mb-30">
+          <hr2>후기 미등록</hr2>
+          <p>아직 등록된 후기가 없습니다.</p>
         </div>
-      </div>
-      <div>
-        <WriteReview />
-      </div>
-    </>
-  );
+      </>
+    );
+  } else {
+    console.log("==> " + avg);
+    return (
+      <>
+        <div className="col mb-30" >
+          <div className="row">
+            <hr2>
+              ★ {avg} | 후기 {list.length}개
+            </hr2>
+          </div>
+          <div
+            className="row"
+            style={{
+              display: "grid",
+              gridTemplateRows: "1fr",
+              gridTemplateColumns: "1fr 1fr",
+            }}
+          >
+            {list.map(
+              ({
+                rv_idx,
+                g_name,
+                g_url,
+                l_name,
+                g_email,
+                rv_content,
+                rv_date,
+                rv_star,
+              }) => (
+                <ReviewItem
+                  rv_idx={rv_idx}
+                  g_name={g_name}
+                  g_url={g_url}
+                  l_name={l_name}
+                  g_email={g_email}
+                  rv_content={rv_content}
+                  rv_date={rv_date}
+                  rv_star={rv_star}
+                  key={rv_idx}
+                />
+              )
+            )}
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default ListReputation;
-
-// export default function Reputation() {
-//   return <ListReputation />;
-// }
+export default Reputation;
