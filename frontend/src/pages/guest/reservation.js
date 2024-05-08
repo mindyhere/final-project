6526178aa65b2b7e1278345195d563d7210ba1bb
JH <hjahee@gmail.com>
2024-05-08 17:21:21 +0900

    [수정] 화면 수정

--- frontend/src/pages/guest/reservation.js
+++ frontend/src/pages/guest/reservation.js
@@ -2,23 +2,24 @@ import Cookies from "universal-cookie";
 import '../../asset/css/user.css';
 import React,{useRef,useEffect,useState} from 'react';
 import PreReservItem from "./preReservItem";
+import LastReservItem from "./lastReservItem";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+
+
+
 function Reservation() {
     const cookies = new Cookies();
     const idx = cookies.get('g_idx');
+    const photo = cookies.get('g_photo');
+
     // const [data, loading] = useFetch('http://localhost/guest/reserv?g_idx='+idx.key);
     const [reservList, setReservList] = useState([]);
-
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+    const [lastReservList, setLastReservList] = useState([]);
+    const idxx = photo.key;
+    console.log(idxx);
 
     function getReserv(url) {
         const form = new FormData();
@@ -28,9 +29,18 @@ function Reservation() {
           return response.json();
     })
     .then(data => {
-        setReservList(data);
+        setReservList(data.after);
+        setLastReservList(data.before);
     })
   }
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
 
   useEffect(() => {getReserv('http://localhost/guest/reserv/list');},[]);
   
@@ -43,32 +53,55 @@ function Reservation() {
                 &nbsp; 여행</h3>
                 <br/>
                 <br/>
+                <div style={{width: "650px"}}>
                 <h4>예정된 예약</h4>
                 <br/>
-                <div>
                 <Slider {...settings}>
-                {reservList.map(
-                ({HoIdx, HoName, HoImg, OCkin, OCkout, HName, HoAddress})=>(
-                    <PreReservItem
-                      HoIdx={HoIdx}
-                      HoName={HoName}
-                      HoImg={HoImg}
-                      OCkin={OCkin}y
-                      OCkout={OCkout}
-                      HName={HName}
-                      HoAddress={HoAddress}
-                    />
-                   
-                )
-            )} 
-            </Slider>
-            </div>
-                <br/>
-                <h4>이전 예약</h4>
+                    {reservList.map(
+                    ({HoIdx, HoName, HoImg, OCkin, OCkout, HName, HoAddress})=>(
+                        <PreReservItem
+                        HoIdx={HoIdx}
+                        HoName={HoName}
+                        HoImg={HoImg}
+                        OCkin={OCkin}
+                        OCkout={OCkout}
+                        HName={HName}
+                        HoAddress={HoAddress}
+                        />
+                    )
+                )} 
+                </Slider>
+                </div> 
+
+                <div style={{marginTop: "50px"}}>
+                    <h4>이전 예약</h4>
+                    <br/>
+                    <div style={{alignContent: 'center', alignItems: 'center'}}>
+                    <div style={{
+                        display:'grid',
+                        gridTemplateRows:'1fr',
+                        gridTemplateColumns:'1fr 1fr 1fr',
+                    }}>
+                    {lastReservList.map(
+                    ({HoIdx, HoName, HoImg, OCkin, OCkout, HName})=>(
+                        <LastReservItem
+                        HoIdx={HoIdx}
+                        HoName={HoName}
+                        HoImg={HoImg}
+                        OCkin={OCkin}
+                        OCkout={OCkout}
+                        HName={HName}
+                        />
+                    )
+                )} 
+      </div>
+      </div>
+                </div>
                  </div>
             </>
         )
     
 }
+
 
 export default Reservation;
