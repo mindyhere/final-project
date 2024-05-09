92adfbed833fc1cf461560111190361e22f349c5
mindy <92miindy@gmail.com>
2024-05-09 15:16:11 +0900

    [test] merge commit test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,6 +28,10 @@ import Order from "./pages/guest/Order";
 import LastReservItem from "./pages/guest/lastReservItem";
 import PreReservItem from "./pages/guest/preReservItem";
 import LastReservDetail from "./pages/guest/lastReservDetail";
+import ReservRevItem from "./pages/guest/reservRevItem";
+import PreReservDetail from "./pages/guest/preReservDetail";
+import CancelReserv from "./pages/guest/cancelReserv";
+
 
 // admin 계정        
 import Amain from "./pages/admin/amain";
@@ -43,6 +47,8 @@ import SearchHostEmail from "./pages/host/login/SearchEmail";
 import SearchHostPw from "./pages/host/login/SearchPw";
 import HostAccount from "./pages/host/hostAccount/HostAccount";
 import EditHostInfo from "./pages/host/hostAccount/EditHostInfo";
+import MyHotelList from "./pages/host/MyHotelList";
+
 
 
 
@@ -73,7 +79,10 @@ function App() {
           <Route path="/guest/Order" element={<Order/>}/>
           <Route path="/guest/preReservItem" element={<PreReservItem/>}/>
           <Route path="/guest/lastReservItem" element={<LastReservItem/>}/>
-          <Route path="/guest/lastReservDetail" element={<LastReservDetail/>}/>
+          <Route path="/guest/lastReservDetail/:OIdx" element={<LastReservDetail/>}/>
+          <Route path="/guest/preReservDetail/:OIdx" element={<PreReservDetail/>}/>
+          <Route path="/guest/reservRevItem" element={<ReservRevItem/>}/>
+          <Route path="/guest/cancelReserv/:OIdx" element={<CancelReserv/>}/>
 
           {/* admin 계정 */}
           <Route path="/admin/amain" element={<Amain />} />
@@ -88,6 +97,7 @@ function App() {
           <Route path="/host/searchPw" element={<SearchHostPw />} />
           <Route path="/api/host/account/:userIdx" element={<HostAccount />} />
           <Route path="/host/edit/:userIdx" element={<EditHostInfo />} />
+          <Route path="/host/hotel/MyHotelList" element={<MyHotelList />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
