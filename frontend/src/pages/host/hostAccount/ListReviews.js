db5b51784c865d58eca15330e4e977760bbad164
mindy <92miindy@gmail.com>
2024-05-10 17:37:41 +0900

    [test] 반복문 에러 수정중

--- frontend/src/pages/host/hostAccount/ListReviews.js
+++ frontend/src/pages/host/hostAccount/ListReviews.js
@@ -15,7 +15,6 @@ function ListReviews() {
   const cookies = new Cookies();
   const userInfo = cookies.get("userInfo");
   const userIdx = userInfo.h_idx;
-  const [loading, setLoading] = useState(false);
   function getList(url) {
     fetch(url)
       .then((response) => {
@@ -25,7 +24,6 @@ function ListReviews() {
         //console.log("==> 리뷰 data? " + JSON.stringify(data));
         setList(data.list);
         // setAvg(data.avgList);
-        setLoading(false);
       });
   }
 
@@ -55,117 +53,127 @@ function ListReviews() {
   //   );
   // }
 
-  if (loading) {
-    return <div>loading...</div>;
-  } else {
-    // console.log(list[0]);
-    return (
-      <>
-        <div id="section1" className="input-group mb-3">
-          <br />
-          <div className="col-6" style={{ float: "rigth" }}></div>
-          <div className="col-6 mb-3" style={{ float: "rigth" }}>
-            <form
-              className="d-flex input-group"
-              id="form1"
-              name="form1"
-              method="post"
-            >
-              <div className="col-3">
-                <div className="input-group d-flex">
-                  <select
-                    className="form-select form-select opt"
-                    id="opt"
-                    style={{
-                      size: "3",
-                      borderRadius: "30px 0 0 30px",
-                      height: "35px",
-                      width: "30%",
-                      textAlign: "left",
-                    }}
-                  >
-                    <option defaultValue={1}>&nbsp;구분</option>
-                    <option defaultValue={2}>&nbsp;최근업로드</option>
-                    <option defaultValue={3}>&nbsp;낮은평점순</option>
-                  </select>
-                </div>
-              </div>
-              <input
-                id="keyword"
-                type="text"
-                className="form-control search"
-                placeholder="검색어를 입력하세요"
-                style={{ height: "35px" }}
-              />
-              <button
-                className="btn main-btn p-0"
-                type="button"
-                id="btnSearch"
-                style={{
-                  height: "35px",
-                  backgroundColor: "#FEC5BB !important",
-                  margin: 0,
-                  padding: "0, 1px 0 1",
-                }}
-              >
-                &nbsp;&nbsp;&nbsp;확인&nbsp;&nbsp;&nbsp;
-              </button>
-            </form>
-          </div>
-        </div>
-        <table
-          id="review"
-          className="table table-sm table-hover align-middle text-center"
-        >
-          <colgroup>
-            <col width="5%" />
-            <col width="10%" />
-            <col width="25%" />
-            <col width="25%" />
-            <col width="15%" />
-            <col width="10%" />
-          </colgroup>
-          <thead>
-            <tr className="align-middle">
-              <th scope="col">
-                <strong>no.</strong>
-              </th>
-              <th scope="col">
-                <strong>구분</strong>
-              </th>
-              <th scope="col">
-                <strong>작성자</strong>
-              </th>
-              <th scope="col">
-                <strong>작성날짜</strong>
-              </th>
-              <th scope="col">
-                <strong>평점</strong>
-              </th>
-              <th scope="col">
-                <strong>답변</strong>
-              </th>
-            </tr>
-          </thead>
-          <tbody
-            className="table-group-divider"
-            style={{ borderColor: "#DBC4F0" }}
+  // console.log(list[0]);
+  return (
+    <>
+      <div id="section1" className="input-group mb-3">
+        <br />
+        <div className="col-6" style={{ float: "rigth" }}></div>
+        <div className="col-6 mb-3" style={{ float: "rigth" }}>
+          <form
+            className="d-flex input-group"
+            id="form1"
+            name="form1"
+            method="post"
           >
-            {list.map((
-              {rv_idx, ho_name}
-            )=>(
-              <ListReviews 
+            <div className="col-3">
+              <div className="input-group d-flex">
+                <select
+                  className="form-select form-select opt"
+                  id="opt"
+                  style={{
+                    size: "3",
+                    borderRadius: "30px 0 0 30px",
+                    height: "35px",
+                    width: "30%",
+                    textAlign: "left",
+                  }}
+                >
+                  <option defaultValue={1}>&nbsp;구분</option>
+                  <option defaultValue={2}>&nbsp;최근업로드</option>
+                  <option defaultValue={3}>&nbsp;낮은평점순</option>
+                </select>
+              </div>
+            </div>
+            <input
+              id="keyword"
+              type="text"
+              className="form-control search"
+              placeholder="검색어를 입력하세요"
+              style={{ height: "35px" }}
+            />
+            <button
+              className="btn main-btn p-0"
+              type="button"
+              id="btnSearch"
+              style={{
+                height: "35px",
+                backgroundColor: "#FEC5BB !important",
+                margin: 0,
+                padding: "0, 1px 0 1",
+              }}
+            >
+              &nbsp;&nbsp;&nbsp;확인&nbsp;&nbsp;&nbsp;
+            </button>
+          </form>
+        </div>
+      </div>
+      <table
+        id="review"
+        className="table table-sm table-hover align-middle text-center"
+      >
+        <colgroup>
+          <col width="5%" />
+          <col width="10%" />
+          <col width="25%" />
+          <col width="25%" />
+          <col width="15%" />
+          <col width="10%" />
+        </colgroup>
+        <thead>
+          <tr className="align-middle">
+            <th scope="col">
+              <strong>no.</strong>
+            </th>
+            <th scope="col">
+              <strong>구분</strong>
+            </th>
+            <th scope="col">
+              <strong>작성자</strong>
+            </th>
+            <th scope="col">
+              <strong>작성날짜</strong>
+            </th>
+            <th scope="col">
+              <strong>평점</strong>
+            </th>
+            <th scope="col">
+              <strong>답변</strong>
+            </th>
+          </tr>
+        </thead>
+        <tbody
+          className="table-group-divider"
+          style={{ borderColor: "#DBC4F0" }}
+        >
+          {list.map(
+            ({
+              rv_idx,
+              ho_name,
+              g_name,
+              g_email,
+              rv_content,
+              rv_date,
+              rv_star,
+              o_idx,
+            }) => (
+              <ListReviews
                 rv_idx={rv_idx}
                 ho_name={ho_name}
+                g_name={g_name}
+                g_email={g_email}
+                rv_content={rv_content}
+                rv_date={rv_date}
+                rv_star={rv_star}
+                o_idx={o_idx}
                 key={rv_idx}
               />
             )
           )}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default ListReviews;
--- frontend/src/pages/host/hostAccount/ReviewItem.js
+++ frontend/src/pages/host/hostAccount/ReviewItem.js
@@ -1,35 +1,35 @@
 import React, { useEffect, useState, useRef } from "react";
 
-function ReviewItem({rv_idx,ho_name}) {
-  // idx, ho_name, g_name, email, g_url, rv_date, rv_star 
-  const [loading, setLoading] = useState(false);
+function ReviewItem({
+  rv_idx,
+  ho_name,
+  g_name,
+  g_email,
+  rv_content,
+  rv_date,
+  rv_star,
+  o_idx,
+}) {
+  let loading = false;
   const idx = useRef();
   // const g_email = useRef();
-  const rendering = (i) => {
-    const star = "⭐";
-    const result = [];
-    for (let j = 0; j < i; j++) {
-      result.push(<span key={j}>{star}</span>);
-    }
-    return result;
-  };
+  // const rendering = (i) => {
+  //   const star = "⭐";
+  //   const result = [];
+  //   for (let j = 0; j < i; j++) {
+  //     result.push(<span key={j}>{star}</span>);
+  //   }
+  //   return result;
+  // };
 
-  console.log("==> loading? " + loading + ", " + rv_idx);
-  setLoading(false);
-
-  if (loading) {
-    console.log("==> 로딩? " + loading + ", " + rv_idx);
-    return <div>loading...</div>;
-  } else {
-    console.log("==> 받기? " + loading + ", " + rv_idx);
-    return (
-      <>
-        <tr className="align-middle">
-          <th scope="row">
-            {ho_name}
-            <input type="hidden" defaultValue={rv_idx} ref={idx} />
-          </th>
-          {/* <td>{ho_name}</td>
+  return (
+    <>
+      <tr className="align-middle">
+        <th scope="row">
+          {ho_name}
+          <input type="hidden" defaultValue={rv_idx} ref={idx} />
+        </th>
+        {/* <td>{ho_name}</td>
       <td>
         {g_name}
         <input type="hidden" defaultValue={email} ref={g_email} readOnly />
@@ -37,9 +37,8 @@ function ReviewItem({rv_idx,ho_name}) {
       <td>{rv_date}</td>
       <td>{rv_star}</td>
       <td>{rp_idx == null ? "미등록" : "등록완료"}</td> */}
-        </tr>
-      </>
-    );
-  }
+      </tr>
+    </>
+  );
 }
 export default ReviewItem;
