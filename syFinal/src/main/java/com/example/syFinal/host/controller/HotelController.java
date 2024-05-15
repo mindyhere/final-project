a8b7a3564198e38cc38ece6e024f97fb122e8f2f
mindy <92miindy@gmail.com>
2024-05-15 10:16:43 +0900

    merge commit

--- syFinal/src/main/java/com/example/syFinal/host/controller/HotelController.java
+++ syFinal/src/main/java/com/example/syFinal/host/controller/HotelController.java
@@ -30,7 +30,7 @@ public class HotelController {
 		map.put("ho_idx", ho_idx);
 		map.put("d_idx", d_idx);
 		Map<String, Object> hotelList = new HashMap<>();
-		hotelList = hotelDao.hoteLlist(map);
+		hotelList = hotelDao.hoteList(map);
 		return hotelList;
 	}
 
@@ -121,26 +121,26 @@ public class HotelController {
 		hotelManagement.put("list", hotelList);
 		return hotelManagement;
 	}
-	
+
 	/* 신규 호텔 등록 */
 	@PostMapping("/host/hotel/registHotel")
-	public void registHotel (@RequestParam Map<String, Object> map,
+	public void registHotel(@RequestParam Map<String, Object> map,
 			@RequestParam(name = "img", required = false) MultipartFile img) {
 		System.out.println("map : " + map);
 		System.out.println("img" + img);
 	}
-	
+
 	/* 호텔 상세 정보 조회 */
 	@GetMapping("/host/hotel/detailMyHotel")
-	public List<Map<String, Object>> detailMyHotel(@RequestParam(name="ho_idx") int ho_idx){
+	public List<Map<String, Object>> detailMyHotel(@RequestParam(name = "ho_idx") int ho_idx) {
 		System.out.println("~~~~~~ ho_idx : " + ho_idx);
-		
+
 		List<Map<String, Object>> detailMyHotel = new ArrayList<>();
 		detailMyHotel = hotelDao.detailMyHotel(ho_idx);
 		return detailMyHotel;
 	}
 	/* 호텔 정보 수정 */
-	
+
 	/* 호텔 삭제 */
-	
+
 }
