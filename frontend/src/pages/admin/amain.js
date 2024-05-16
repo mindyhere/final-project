eb95d4bb65bbebc31074235a4dec7ec917a2e562
양미영 <158830825+didaldud@users.noreply.github.com>
2024-05-16 14:16:48 +0900

    [수정]관리자-로그인 기능/사업자 페이지 오류 수정 (#196)
     [수정]관리자-로그인 기능/사업자 페이지 오류

--- frontend/src/pages/admin/amain.js
+++ frontend/src/pages/admin/amain.js
@@ -1,15 +1,15 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from "universal-cookie";
-import { HouseFill, JustifyLeft, PersonWorkspace, PersonCircle, PersonVcard, PersonFill, HouseGear, HouseCheck, Houses, HouseDoor } from "react-bootstrap-icons";
+import { PersonWorkspace, PersonFill, PersonCircle, PersonVcard, HouseGear, HouseCheck, Houses, HouseDoor, JustifyLeft } from "react-bootstrap-icons";
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useSearchParams } from "react-router-dom";
-import { useNavigate } from "react-router";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 function Amain() {
+  
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const navigate = useNavigate();
-   
+    
 
     useEffect(() => {
         const cookies = new Cookies();
@@ -17,6 +17,7 @@ function Amain() {
         const a_passwd_cookie = cookies.get('a_passwd');
         if (a_id_cookie && a_passwd_cookie) {
             setIsLoggedIn(true);
+            
         }
     }, []);
 
@@ -29,7 +30,8 @@ function Amain() {
             navigate('/admin/amain');
         }
     };
-    const handlelogin = () => {
+
+    const handleLogin = () => {
         if (window.confirm('로그인 페이지로 이동하시겠습니까?')) {
             setIsLoggedIn(false);
             navigate('/admin/alogin');
@@ -57,64 +59,63 @@ function Amain() {
         }
     };
 
+
+
+    const locationNow = useLocation()
+    if (locationNow.pathname === "/admin/alogin") return null; 
+
     return (
         <>
-            <nav className="navbar bg-body-tertiary fixed-top">
-                <a className="navbar-brand">
-                    <PersonWorkspace width="50" height="50" />&nbsp; 관리자 페이지
-                </a>
-                {!isLoggedIn? <button onClick={handleLogout} className="btn">로그아웃</button> : <button onClick={handlelogin} className="btn">로그인</button>} 
-            </nav>
-            <br /><br />
-            <div className="row">
-                <div className="col-4">
-                    <nav id="navbar-example3" className="h-100 flex-column align-items-stretch pe-4 border-end">
-                        <nav className="nav nav-pills flex-column">
-                            <a className="nav-link" href="#item-1" /><br />
-                            <a className="nav-link ms-3 my-1" onClick={(e) => !Adminout(e, '/')}>
-                                <HouseDoor width="30" height="30" />&nbsp; home
-                            </a>                        
-                            <NavDropdown title={<>&nbsp;&nbsp;<PersonFill width="30" height="30" />&nbsp;회원정보관리</>}>
-                                <NavDropdown.Item className="btn" href="./aguest" onClick={(e) => AdminClick(e, '/admin/aguest')}><PersonCircle width="30" height="30" />&nbsp; 이용자 정보관리</NavDropdown.Item>
-                                <NavDropdown.Item  className="btn" href="./ahost" onClick={(e) => AdminClick(e, '/admin/ahost')}><PersonVcard width="30" height="30" />&nbsp; 사업자 정보관리</NavDropdown.Item>
-                            </NavDropdown>
-                            <NavDropdown title={<>&nbsp;&nbsp;<HouseGear width="30" height="30" />&nbsp; 숙박 운영관리</>}>
-                                <NavDropdown.Item className="btn" href="./" onClick={(e) => AdminClick(e, '/admin/amain')}><Houses width="30" height="30" />&nbsp; 숙박 리스트</NavDropdown.Item>
-                                <NavDropdown.Item  className="btn" href="./" onClick={(e) => AdminClick(e, '/admin/amain')}><HouseCheck width="30" height="30" />&nbsp; 숙박 등록 승인</NavDropdown.Item>
-                            </NavDropdown>                           
-                            <a className="nav-link ms-3 my-1" href="./notice/alist" onClick={(e) => AdminClick(e, '/admin/notice/alist')}>
-                                <JustifyLeft width="30" height="30" />&nbsp; 공지사항
-                            </a>
-                        </nav>
-                    </nav>
-                </div>
-                {/* 메인 구성 */}
-                <div className="col-xl-4 col-lg-5">
-                    <br /><br />
-                    <div className="card shadow mb-4">
-                        <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                            <h6 className="m-0 font-weight-bold text-primary">캘린더</h6>
-                        </div>
-                        <div className="card-body">
-                            {/* <FullCalendar
-                                plugins={[dayGridPlugin]}
-                                initialView="dayGridMonth"
-                            /> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-4 col-lg-5">
-                        <div className="card shadow mb-4">
-                            <div className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 className="m-0 font-weight-bold text-primary">지도</h6>
-                            </div>
-                            <div className="card-body">
+        <div className="p-3 mb-3 border-bottom">
+            <div className="container">
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+                        <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                            <use xlinkHref="#bootstrap"/>
+                        </svg>
+                    </a>
 
-                            </div>
-                        </div>
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                        <li><a href="/admin/amain" className="nav-link px-2 link-secondary"> <HouseDoor size={30} />&nbsp;HOME</a></li>
+                        <li className="nav-item">
+                            <NavDropdown title={<>&nbsp;&nbsp;<PersonFill size={30} />&nbsp; 회원정보관리</>}>
+                                <NavDropdown.Item href="./aguest" onClick={(e) => AdminClick(e, '/admin/aguest')}><PersonCircle size={30} />&nbsp; 이용자 정보관리</NavDropdown.Item>
+                                <NavDropdown.Item href="./ahost" onClick={(e) => AdminClick(e, '/admin/ahost')}><PersonVcard size={30} />&nbsp; 사업자 정보관리</NavDropdown.Item>
+                            </NavDropdown>
+                        </li>
+                        <li className="nav-item">
+                            <NavDropdown title={<>&nbsp;&nbsp;<HouseGear size={30} />&nbsp; 숙박 운영관리</>}>
+                                <NavDropdown.Item href="./" onClick={(e) => AdminClick(e, '/admin/amain')}><Houses size={30} />&nbsp; 숙박 리스트</NavDropdown.Item>
+                                <NavDropdown.Item href="./" onClick={(e) => AdminClick(e, '/admin/amain')}><HouseCheck size={30} />&nbsp; 숙박 등록 승인</NavDropdown.Item>
+                            </NavDropdown>
+                        </li>
+                        <li><a  href="./notice/alist" onClick={(e) => AdminClick(e, '/admin/notice/alist')} className="nav-link px-2 link-body-emphasis"> <JustifyLeft size={30} /> &nbsp;게시판</a></li>
+                    </ul>
+
+                    <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                        <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/>
+                    </form>
+
+                    <div className="dropdown text-end">
+                        <a href="#" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="/img/whitedog.jpg" width="40" height="40" className="rounded-circle"/>
+                        </a>
+                        <ul className="dropdown-menu text-small">
+                            {!isLoggedIn ? (
+                                <button onClick={handleLogout} className="dropdown-item" type="button"><p>* 관리자님 환영합니다 *</p> <hr/>Sign out</button>
+                              
+                            ) : (
+                                <button onClick={handleLogin} className="dropdown-item" type="button">Sign in</button>
+                            )}
+                            <hr className="dropdown-divider"/>
+                            <li><a className="dropdown-item" onClick={(e) => !Adminout(e, '/')}>사이트맵</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
+        </div>
         </>
-    )
+    );
 }
+
 export default Amain;
