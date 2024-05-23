dc6c4664087937822cf9914189e2dfff5025ae42
양미영 <158830825+didaldud@users.noreply.github.com>
2024-05-23 09:35:23 +0900

    [수정] 관리자/ 사업자 가입승인 절차 수정 (#248)
    * [init] 브랜치 생성
    
    * [수정]관리자-로그인 기능/사업자관리 페이지 오류
    
    * [수정]관리자-로그인 기능/사업자,공지 페이지 수정
    
    * [테스트]
    
    * [수정]관리자-로그인 기능/사업자 가입승인 오류수정
    
    * [수정] 관리자/로그인 기능 오류 수정
    
    * [스타일] 관리자/ 페이지 디자인 적용
    
    * [오류] 관리자 / 디자인 적용 오류 수정
    
    * [오류] 관리자/ 디자인 적용 오류
    
    * [추가] 관리자/ 호텔 리스트 페이지
    
    * [수정] 관리자/ 경로 코드 오류
    
    * [수정] 관리자/ 호텔 페이지 경로 오류
    
    * [수정] 관리자- 신규 회원 자동적립 기능/ 사업자 가입 승인  체크
    
    * [수정] 관리자/  사업자 가입승인 절차 수정

--- syFinal/src/main/java/com/example/syFinal/admin/model/dao/AhostDAO.java
+++ syFinal/src/main/java/com/example/syFinal/admin/model/dao/AhostDAO.java
@@ -8,6 +8,6 @@ public interface AhostDAO {
 	List<AHostDTO> list(String searchkey, String search);
 	String delete(int h_idx);
 	AHostDTO detail(int g_idx);
-	void a_approve(int h_idx);
+	void a_approve(int h_idx); 
 	AHostDTO check_file(int h_idx);
 } 
