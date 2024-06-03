953c0fb0fa4f35ed1b0624d3fe5f75a16bfb9f90
MY <didaldud5246@naver.com>
2024-06-03 09:20:24 +0900

    [수정] 관리자 메인페이지 디자인 수정

--- frontend/src/pages/admin/amain.js
+++ frontend/src/pages/admin/amain.js
@@ -5,10 +5,12 @@ import MemoList from './memolist';
 import HotelChart from './chart';
 import Sidebar from './sidebar';
 import Calendar from 'react-calendar';
-// import '../admin/css/astyles.css';
+import '../admin/css/astyles.css';
 
 function Amain() {
   const [searchDate, setSearchDate] = useState(new Date());
+  const nextWeek = new Date(searchDate);
+  nextWeek.setDate(nextWeek.getDate() + 1);
 
   return (
     <div className="container-fluid">
@@ -22,41 +24,42 @@ function Amain() {
           </div>
           <hr />
           <main>
-            <div className="main-content">
-              <div className="row">
-                <div className="col-lg-8 mb-4">
-                  <div className="card h-70 d-flex flex-column">
-                    <div className="card-header"  style={{  backgroundColor: '#4e817269'}}>
+          <div className="main-content">
+              <div className="row justify-content-center">
+                <div className="col-lg-4 mb-4">
+                  <div className="card h-60">
+                    <div className="card-header" style={{ backgroundColor: '#4e817269' }}>
                       Calendar
                     </div>
                     <div className="card-body d-flex flex-column">
-                      <div className="mb-4">
+                      <div className="mb-3">
                         <strong>오늘 날짜:</strong> {searchDate.toLocaleDateString()}
                       </div>
-                      <div className="flex-grow-1">
-                        <Calendar className="cute-calendar" value={searchDate} />
+                      <div className="flex-grow-1 d-flex justify-content-center">
+                        <Calendar className="calendar1" value={searchDate} />
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="col-lg-4 mb-4">
-                  <div className="card h-70 d-flex flex-column">
-                    <div className="card-body d-flex flex-column">
-                      <div className="flex-grow-1">
-                        <MemoList />
+                <div className="col-lg-7 mb-4">
+                  <div className="row">
+                    <div className="col-lg-12 mb-4" >                    
+                    <div className="flex-grow-1 text-left">
+                        <MemoList />                    
                       </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row">
+                         </div>
+                  </div>               
+                    <div className="row">
                 <div className="col-lg-12 mb-8">
-                  <div className="card h-150">
+                  <div className="card h-100">
                     <div className="card-header" style={{  backgroundColor: '#4e817269'}}>
                       호텔 월 총 매출
                     </div>
                     <div className="card-body">
                       <HotelChart />
+                    </div>
+ 
                     </div>
                   </div>
                 </div>
